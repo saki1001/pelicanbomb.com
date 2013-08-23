cc5688d2e34395e9a4895c471f8720895f200490
Saki Sato <sakiisato@gmail.com>
2013-08-23 18:38:44 -0400

    fleshing out home page

--- wp-content/themes/cleanslate/index.php
+++ wp-content/themes/cleanslate/index.php
@@ -14,6 +14,9 @@
         <?php
             if ( have_posts() ) :
                 
+                $readCategoryID = get_cat_ID('read');
+                $seeCategoryID = get_cat_ID('see');
+                
                 $readImage = get_home_image('read', 'full');
                 // $collectImage = get_home_image('collect', 'large');
                 // $seeImage = get_home_image('see', 'large');
@@ -21,21 +24,21 @@
             
             <div id="primary">
                 <section class="column read">
-                    <a href="#">
+                    <a href="<?php echo get_category_link($readCategoryID); ?>">
                         <h2>Read</h2>
                         <img src="<?php echo $readImage[0]; ?>" width="<?php echo $readImage[1]; ?>" height="<?php echo $readImage[2]; ?>" alt="Read" />
                     </a>
                 </section>
                 
                 <section class="column collect">
-                    <a href="#">
+                    <a href="http://www.thedropnola.com/">
                         <h2>Collect</h2>
                         <!-- <img src="<?php echo $readImage[0]; ?>" width="<?php echo $readImage[1]; ?>" height="<?php echo $readImage[2]; ?>" alt="Read" /> -->
                     </a>
                 </section>
                 
                 <section class="column see">
-                    <a href="#">
+                    <a href="<?php echo get_category_link($seeCategoryID); ?>">
                         <h2>See</h2>
                         <!-- <img src="<?php echo $readImage[0]; ?>" width="<?php echo $readImage[1]; ?>" height="<?php echo $readImage[2]; ?>" alt="Read" /> -->
                     </a>
@@ -48,11 +51,8 @@
                     <h2>Featured Articles</h2>
                     
                     <?php
-                        
-                        $featuredArticles = get_featured_posts('read', 1);
-                        
-                        print_r($featuredArticles);
-                        
+                        // Get Featured Read Articles
+                        echo get_featured_posts('read', 3);
                     ?>
                     
                 </section>
