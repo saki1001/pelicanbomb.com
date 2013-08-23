dc4b7fa64284099c2f7d90e6c74eb379b9bc5109
Saki Sato <sakiisato@gmail.com>
2013-08-23 17:18:30 -0400

    added article tag

--- wp-content/themes/cleanslate/content-thumb.php
+++ wp-content/themes/cleanslate/content-thumb.php
@@ -7,20 +7,25 @@
  */
 ?>
 
-<a href="<?php the_permalink(); ?>">
-    <div class="caption">
-        <h4 class="post-title">
-            <?php the_title(); ?>
-        </h4>
-        <p class="post-date">
-            <?php echo get_the_date(); ?>
-        </p>
-        <p class="post-excerpt">
-            <?php echo substr(get_the_excerpt(), 0,225); ?>
-        </p>
-    </div>
+<article class="preview">
+    <a href="<?php the_permalink(); ?>">
+        <div class="caption">
+            <h4 class="post-title">
+                <?php the_title(); ?>
+            </h4>
+            <p class="post-date">
+                <?php echo get_the_date(); ?>
+            </p>
+            <p class="post-excerpt">
+                <?php echo substr(get_the_excerpt(), 0,225); ?>
+            </p>
+        </div>
     
-    <figure class="post-thumb">
-        <img src="<?php echo get_thumbnail_custom($post->ID, 'browse-thumbnail'); ?>" width="392" height="392" alt="<?php the_title(); ?>" />
-    </figure>
-</a>
+        <figure class="post-thumb">
+            <?php
+                $thumb = get_thumbnail_custom($post->ID, 'thumbnail');
+            ?>
+            <img src="<?php echo $thumb[0]; ?>" width="<?php echo $thumb[1]; ?>" height="<?php echo $thumb[2]; ?>" alt="<?php the_title(); ?>" />
+        </figure>
+    </a>
+</article>
