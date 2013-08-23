fce7f66465bccb6fbf9076b1cad65576e7fc3459
Saki Sato <sakiisato@gmail.com>
2013-08-23 18:36:03 -0400

    sidebar for sorting with child categories

---
+++ wp-content/themes/cleanslate/sidebar-sort.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The Sidebar for sorting by sub-categories.
+ *
+ * @package CleanSlate
+ * @since CleanSlate 0.1
+ */
+?>
+
+<?php
+    // Query Category Object
+    $cat_obj = $wp_query->get_queried_object();
+    $currentCategory = $cat_obj->cat_name;
+    $class = '';
+    
+    // For Child Categories
+    if( $cat_obj->parent ) :
+        $parentCategoryID = $cat_obj->parent;
+        
+    // For the Parent Category
+    else :
+        $parentCategoryID =  $cat_obj->term_id;
+        $class = "class='current'";
+    endif;
+?>
+
+<section id="sidebar">
+    
+    <h2>Sort by</h2>
+    
+    <ul>
+        <li <?php echo $class; ?>>
+            <a href="<?php echo get_category_link( $parentCategoryID ); ?>">View All</a>
+        </li>
+        
+    <?php
+        // Get categories, exclude misc. Read categories
+        $args = array(
+            'child_of' => $parentCategoryID,
+            'exclude' => '7,8,9,10,11,12,15,18,6',
+            'hide_empty'  => 1
+        );
+        
+        $childCategories = get_categories( $args );
+        
+        foreach( $childCategories as $childCategory ) :
+            $class = '';
+            
+            if ($currentCategory === $childCategory->cat_name) :
+                $class = "class='current'";
+            endif;
+        ?>
+            <li <?php echo $class; ?>>
+                <a href="<?php echo get_category_link( $childCategory->cat_ID ); ?>"><?php echo $childCategory->cat_name; ?></a>
+            </li>
+    <?php
+        endforeach;
+    ?>
+    </ul>
+</section><!-- #sidebar -->
