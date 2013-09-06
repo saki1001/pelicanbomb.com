6c3473ab0e1d63e75d999ebc9ad1e39a07b39a91
Saki Sato <sakiisato@gmail.com>
2013-09-06 18:16:51 -0400

    added searchform

---
+++ wp-content/themes/cleanslate/searchform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The search form.
+ *
+ * @package CleanSlate
+ * @since CleanSlate 0.1
+ */
+?>
+
+<form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+    <label class="screen-reader-text" for="s">Search for:</label>
+    <input type="text" value="Search..." name="s" id="s" placeholder="Search..." />
+    <input type="submit" id="s-submit" name="submit" value="Go" />
+</form>
