75928e470edb860dc72a297a6eaafc04b22ad818
Saki Sato <sakiisato@gmail.com>
2013-09-06 18:17:45 -0400

    took code from calamara site and customized styling, bust basically the same

---
+++ wp-content/themes/cleanslate/js/slideshow.js
@@ -0,0 +1,201 @@
+$j(document).ready(function() {
+    
+    /* POSITION IMAGES
+    ------------------------------------------- */
+    
+    var imagesArray = [];
+    var fullWidth = 0;
+    
+    // get full width
+    $j('#scroll .image-container').each(function() {
+        
+        w = $j(this).outerWidth();
+        // adding w + 1 because width is rounded
+        fullWidth += w + 1;
+    });
+    
+    // set correct width
+    $j('#scroll').css('width', fullWidth);
+    
+    // now determine positions
+    $j('#scroll .image-container').each(function(i) {
+        
+        var position = $j(this).position();
+        
+        $j(this).attr('data-index', i);
+        
+        imagesArray.push({
+            pos: position.left,
+            w: $j(this).width(),
+            h: $j(this).height()
+        });
+    
+    });
+    
+    // add pager elements
+    $j.each(imagesArray, function(i) {
+        var pagerNumber = i + 1;
+        var imgPosition = this.pos;
+        $j('#pager').append('<a href="#" class="page" data-index="' + i + '" data-position="' + imgPosition + '">' + pagerNumber + '</a>');
+        
+        if(pagerNumber < imagesArray.length) {
+            $j('#pager a').last().after('<span>&nbsp;|&nbsp;</span>')
+        }
+        
+    });
+    
+    // get pager width
+    var pagerWidth = 0;
+    $j('#pager').children().each(function() {
+        var pw = $j(this).outerWidth(true);
+        pagerWidth += pw;
+    });
+    
+    // now set width of pager element
+    // for centering pager links
+    $j('#pager').css('width', pagerWidth);
+    
+    // find first and last image
+    $j('.image-container').each(function(i) {
+        
+        var imageIndex = $j(this).attr('data-index');
+        var lastIndex = String(imagesArray.length - 1);
+        
+        if(imageIndex === '0') {
+            $j(this).addClass('firstSlide');
+        }
+        
+        if(imageIndex === lastIndex) {
+            $j(this).addClass('lastSlide');
+        }
+    });
+    
+    var labelCurrentPosition = function(num) {
+        
+        // remove old previousSlide class
+        $j('.previousSlide').removeClass('previousSlide');
+        
+        // add new previousSlide
+        $j('.currentSlide').addClass('previousSlide');
+        
+        // remove old currentSlide
+        $j('.currentSlide').removeClass('currentSlide');
+        
+        // add new currentSlide
+        $j('.page[data-index="' + num +'"]').addClass('currentSlide');
+        $j('#scroll .image-container[data-index="' + num +'"]').addClass('currentSlide');
+        
+        // clear any inactive classes
+        $j('.nav.inactive').removeClass('inactive');
+        
+        // make arrows inactive for first/last slide
+        if ($j('.currentSlide').hasClass('firstSlide')) {
+            $j('.nav.prev').addClass('inactive');
+        }
+        
+        if ($j('.currentSlide').hasClass('lastSlide')) {
+            $j('.nav.next').addClass('inactive');
+        }
+        
+    };
+    
+    // for document load
+    labelCurrentPosition(0);
+    
+    var animateScroll = function(pos, time) {
+        
+        $j('#scroll .image-container.previousSlide').animate({
+            opacity: 0.25
+        }, 200 , function() {
+            // animation complete
+        });
+        
+        $j('#scroll').animate({
+            left: pos
+        }, time , function() {
+            // animation complete
+            $j('#scroll .image-container.currentSlide').animate({
+                opacity: 1
+            }, 200 , function() {
+                // animation complete
+            });
+            
+        });
+        
+    };
+    
+    var showImage = function() {
+        
+        var curDataIndex = $j(this).attr('data-index');
+        labelCurrentPosition(curDataIndex);
+        
+        var positionLeft = '-' + $j(this).attr('data-position') + 'px';
+        var curPosition = $j('#scroll').position();
+        
+        // base animation time on ratio of how far you're moving
+        var animTime = Math.abs(Math.abs(parseInt(curPosition.left)) - Math.abs(parseInt(positionLeft)))/2;
+        
+        animateScroll(positionLeft, animTime);
+        
+        return false;
+    };
+    
+    var showPrevNext = function() {
+        
+        var curDataIndex = parseInt($j('.page.currentSlide').attr('data-index'));
+        
+        if ($j(this).hasClass('prev')) {
+            var newDataIndex = curDataIndex - 1;
+        } else {
+            var newDataIndex = curDataIndex + 1;
+        }
+        
+        var showImage = '.page[data-index="' + newDataIndex + '"]';
+        
+        if ( $j(showImage)[0] != null ) {
+            
+            var positionLeft = '-' + $j(showImage).attr('data-position') + 'px';
+            
+            labelCurrentPosition(newDataIndex);
+            animateScroll(positionLeft, 300);
+            
+        }
+        
+        return false;
+    };
+    
+    var showNav = function() {
+        $j('.nav').fadeIn(100);
+    };
+    
+    var hideNav = function() {
+        $j('.nav').fadeOut(100);
+    };
+    
+    
+    /* CENTER IMAGES
+    ------------------------------------------- */
+    
+    // Set Image Width and Margin
+    // for centering images in portfolio pages
+    $j('#media .image-container figure img').each(function(i) {
+        
+        var width = $j(this).width();
+        var height = $j(this).height();
+        var marginTop = (400 - height)/2 + 'px';
+        
+        // add marginTop and negative marginTop to bottom
+        // to prevent container from becoming too large
+        $j(this).css({
+            'width': width,
+            'padding-top': marginTop
+        });
+    });
+    
+    $j('#pager .page').on('click', showImage);
+    $j('.border .arrow').on('click', showPrevNext);
+    $j('#media').on({
+        'mouseenter': showNav,
+        'mouseleave': hideNav
+    });
+});
