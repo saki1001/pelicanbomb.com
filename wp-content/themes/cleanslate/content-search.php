f393951dc84e217bb8e0e7fd50c8bee5c538aff8
Saki Sato <sakiisato@gmail.com>
2013-09-09 15:17:35 -0400

    template for search page thumbnails

---
+++ wp-content/themes/cleanslate/content-search.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template to display thumbnails.
+ *
+ * @package CleanSlate
+ * @since CleanSlate 0.1
+ */
+?>
+
+<?php include('php/get-post-meta.php'); ?>
+
+<article class="search">
+    <a href="<?php the_permalink(); ?>">
+        <p class="post-category">
+            <?php echo $categoryName; ?>
+        </p>
+        <p class="post-date">
+            <?php echo get_the_date(); ?>
+        </p>
+        <h4 class="post-title">
+            <?php the_title(); ?>
+        </h4>
+        <p class="post-author">
+            By <?php echo $author; ?>
+        </p>
+        <p class="post-excerpt">
+            <?php echo the_excerpt_max_charlength(180); ?>
+        </p>
+    </a>
+</article>
