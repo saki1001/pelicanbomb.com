33f488b22a9f92d0097ad20d382b5baa99ef287f
Saki Sato <sakiisato@gmail.com>
2013-09-09 15:20:31 -0400

    minor additions, like start end date

--- wp-content/themes/cleanslate/content-summary.php
+++ wp-content/themes/cleanslate/content-summary.php
@@ -7,25 +7,53 @@
  */
 ?>
 
+<?php include('php/get-post-meta.php'); ?>
+
+<?php
+    if( !$categoryName ) {
+        $categoryName = 'Exhibition';
+    }
+?>
+
 <article class="summary">
     <a href="<?php the_permalink(); ?>">
-        <div class="caption">
-            <h4 class="post-title">
-                <?php the_title(); ?>
-            </h4>
-            <p class="post-date">
-                <?php echo get_the_date(); ?>
-            </p>
-            <p class="post-excerpt">
-                <?php echo substr(get_the_excerpt(), 0,225); ?>
-            </p>
-        </div>
-    
         <figure class="post-thumb">
             <?php
                 $thumb = get_thumbnail_custom($post->ID, 'thumbnail');
             ?>
             <img src="<?php echo $thumb[0]; ?>" width="<?php echo $thumb[1]; ?>" height="<?php echo $thumb[2]; ?>" alt="<?php the_title(); ?>" />
         </figure>
+        
+        <div class="caption">
+            <p class="post-category">
+                <?php echo $categoryName; ?>
+            </p>
+            <h4 class="post-title">
+                <?php the_title(); ?>
+            </h4>
+            <p class="post-date">
+                <?php
+                    // Get Start and End dates
+                    if( in_category('see') ) {
+                        $startDate = date('F j, Y', strtotime(get_field('start-date')));
+                        
+                        if( get_field('end-date') ) {
+                            $endDate = date('F j, Y', strtotime(get_field('end-date')));
+                            
+                            $date = $startDate . ' - ' . $endDate;
+                        } else {
+                            $date = $startDate;
+                        }
+                    } else {
+                        $date = get_the_date();
+                    }
+                    
+                    echo $date;
+                ?>
+            </p>
+            <p class="post-excerpt">
+                <?php echo the_excerpt_max_charlength(200); ?>
+            </p>
+        </div>
     </a>
 </article>
