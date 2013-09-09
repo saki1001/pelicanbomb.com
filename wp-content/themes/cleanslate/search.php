ecebff5534cc943f0e14046aad50640c3f05ae46
Saki Sato <sakiisato@gmail.com>
2013-09-09 15:24:55 -0400

    added pagination and some container elements, also a search form at top, and removed form in footer

--- wp-content/themes/cleanslate/search.php
+++ wp-content/themes/cleanslate/search.php
@@ -13,20 +13,36 @@
         
         <h2>Search Results for <span>&ldquo;<?php the_search_query() ?>&rdquo;</span></h2>
         
+        <div class="sub-header">
+            <?php get_search_form(); ?>
+        </div>
+        
+        <?php get_sidebar(); ?>
+        
         <?php
             
             if ( have_posts() ) :
-                
+        ?>
+        
+        <div id="articles">
+            
+        <?php
                 while ( have_posts() ) : the_post();
-                    get_template_part('content-preview', get_post_format() );
+                    get_template_part('content-search' );
                 endwhile;
-                
+        ?>
+        
+        </div>
+        
+        <?php
             else :
                 // Content Not Found Template
                 include('content-no-results.php');
                 
             endif;
         ?>
+        
+        <?php get_template_part( 'content-pagination' ); ?>
         
     </section>
     
