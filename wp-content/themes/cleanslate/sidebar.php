998ac2d17ae9f4a3aa117cff128944f953441263
Saki Sato <sakiisato@gmail.com>
2013-09-13 17:42:02 -0400

    added related events section

--- wp-content/themes/cleanslate/sidebar.php
+++ wp-content/themes/cleanslate/sidebar.php
@@ -21,6 +21,12 @@
         $featuredName = 'Featured Article';
         $showAds = false;
         
+    } elseif ( is_home() ) {
+        
+        $featuredCat = 'see';
+        $featuredName = 'Featured Exhibition';
+        $showAds = true;
+        
     } else {
         
         $featuredCat = 'events';
@@ -30,6 +36,38 @@
     }
 ?>
     <div id="sidebar">
+        
+        <?php
+            // Related Events
+            if( is_single() && in_category('see') ){
+                
+                $relatedEventsIDs = get_field('related_events');
+                
+                $related_events_args = array(
+                    'category_name' => 'events',
+                    'post__in' => $relatedEventsIDs,
+                    'meta-key' => 'start-date',
+                    'orderby' => 'meta-value'
+                );
+                
+                $related_events_query = new WP_Query( $related_events_args );
+                
+                if ( $related_events_query->have_posts() ) :
+        ?>
+                <section class="related-events">
+                    <h3>Related Events</h3>
+        <?php
+                    while ( $related_events_query->have_posts() ) : $related_events_query->the_post();
+                        get_template_part( 'content-related', get_post_format() );
+                    endwhile;
+        ?>
+                </section>
+        <?php
+                else :
+                    // do nothing
+                endif;
+            }
+        ?>
         
         <section class="featured-post">
             <h3>
