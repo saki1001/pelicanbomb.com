3e55b2f4ec8c6bf9667d646b761c41776c2ef084
Saki Sato <sakiisato@gmail.com>
2013-09-23 18:38:56 -0400

    organized sections and added advertisements section

--- wp-content/themes/cleanslate/sidebar.php
+++ wp-content/themes/cleanslate/sidebar.php
@@ -18,7 +18,7 @@
     } elseif ( is_page() ) {
         
         $featuredCat = 'read';
-        $featuredName = 'Featured Article';
+        $featuredName = 'Featured Product';
         $showAds = false;
         
     } elseif ( is_home() ) {
@@ -35,63 +35,113 @@
         
     }
 ?>
-    <div id="sidebar">
+
+<div id="sidebar">
+
+<?php
+    // ELEMENT 1
+    // Related Posts
+    if( is_single() && (in_category('see') || in_category('read')) ){
         
-        <?php
-            // Related Events
-            if( is_single() && in_category('see') ){
-                
-                $relatedEventsIDs = get_field('related_events');
-                
-                $related_events_args = array(
-                    'category_name' => 'events',
-                    'post__in' => $relatedEventsIDs,
-                    'meta-key' => 'start-date',
-                    'orderby' => 'meta-value'
-                );
-                
-                $related_events_query = new WP_Query( $related_events_args );
-                
-                if ( $related_events_query->have_posts() ) :
-        ?>
-                <section class="related-events">
-                    <h3>Related Events</h3>
-        <?php
-                    while ( $related_events_query->have_posts() ) : $related_events_query->the_post();
-                        get_template_part( 'content-related', get_post_format() );
-                    endwhile;
-        ?>
-                </section>
-        <?php
-                else :
-                    // do nothing
-                endif;
-            }
-        ?>
+        $sectionTite = ( in_category('see') ? 'events' : 'articles' );
         
-        <section class="featured-post">
-            <h3>
-                <?php echo $featuredName; ?>
-            </h3>
-            
-            <?php
-                // Get Featured Post
-                echo get_featured_posts($featuredCat, 1, 'content-feature');
-            ?>
-            
+        $relatedPostsIDs = get_field('related-posts');
+        
+        $related_posts_args = array(
+            'post__in' => $relatedPostsIDs,
+            'orderby' => 'date'
+        );
+        
+        $related_posts_query = new WP_Query( $related_posts_args );
+        
+        if ( $relatedPostsIDs != '' && $related_posts_query->have_posts() ) :
+?>
+        <section id="related">
+            <h3>Related <?php echo ucfirst($sectionTite); ?></h3>
+<?php
+            while ( $related_posts_query->have_posts() ) : $related_posts_query->the_post();
+                get_template_part( 'content-related', get_post_format() );
+            endwhile;
+?>
         </section>
 <?php
-    if( !is_page() ) :
+        else :
+            // do nothing
+        endif;
+    }
+?>
+
+<?php
+    // ELEMENT 2
+    // Featured Post/Product
+?>
+    <section class="featured-post">
+        <h3>
+            <?php echo $featuredName; ?>
+        </h3>
+        
+<?php
+    // For Pages
+    // Content loaded dynamically
+    if( is_page() ){
+?>
+        
+        <div class="loading"></div>
+        
+<?php
+    // For Everything else
+    // Get Featured Post
+    } else {
+        
+        echo get_featured_posts($featuredCat, 1, 'content-feature');
+        
+    }
+?>
+    </section>
+    
+<?php
+    // ELEMENT 3
+    // Advertisements
+    if( $showAds === true ) :
+        
+        $page = get_page_by_title( 'Advertisements' );
+        $ads = get_field('advertisement', $page->ID);
+        
+        if( $ads != '' ) {
 ?>
         <div class="ads">
+<?php
+        $i = 0;
+        
+        // Iterate throught Ads
+        foreach( $ads as $ad ) :
             
-            <div class="ad left"></div>
-            <div class="ad right"></div>
-            <div class="ad left"></div>
-            <div class="ad right"></div>
+            // Find odd or even
+            if( $i %2 == 0) {
+                $class = 'left';
+            } else {
+                $class = 'right';
+            }
             
+            $thumb = array(
+                'url' => $ad['ad-image']['sizes']['small-thumbnail'],
+                'width' => $ad['ad-image']['sizes']['small-thumbnail-width'],
+                'height' => $ad['ad-image']['sizes']['small-thumbnail-height']
+            );
+?>
+            <div class="ad <?php echo $class; ?>">
+                <a href="<?php echo $ad['ad-link']; ?>" title="<?php echo $ad['ad-title']; ?>" target="_blank">
+                    <img src="<?php echo $thumb['url']; ?>" width="<?php echo $thumb['width']; ?>" height="<?php echo $thumb['height']; ?>" alt="<?php echo $ad['ad-title']; ?>" />
+                </a>
+            </div>
+<?php
+            $i++;
+        
+        endforeach;
+?>
         </div>
 <?php
+        }
     endif;
 ?>
-    </div><!-- #sidebar -->
+</div><!-- #sidebar -->
