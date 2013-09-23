af98b208589577a2c4c2bea004ff7b3b21a13f2c
Saki Sato <sakiisato@gmail.com>
2013-09-23 18:24:43 -0400

    added category and date to related content

--- wp-content/themes/cleanslate/content-related.php
+++ wp-content/themes/cleanslate/content-related.php
@@ -7,19 +7,20 @@
  */
 ?>
 
+<?php include('php/get-post-meta.php'); ?>
 <?php include('php/get-event-meta.php'); ?>
 
 <article class="related">
     <a href="<?php the_permalink(); ?>">
         <div class="caption">
-            <p class="post-date">
-                <?php echo $date; ?>
+            <p class="post-category">
+                <?php echo $categoryName; ?>
             </p>
             <h4 class="post-title">
                 <?php the_title(); ?>
             </h4>
-            <p class="location">
-                <?php echo $address; ?>
+            <p class="post-date">
+                <?php echo $date; ?>
             </p>
         </div>
     </a>
