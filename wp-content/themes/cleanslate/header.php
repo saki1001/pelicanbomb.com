823a8ccb7e133eb04536faa890a6e853f9b8c18b
Saki Sato <sakiisato@gmail.com>
2013-10-08 17:44:01 -0400

    changed ie conditional

--- wp-content/themes/cleanslate/header.php
+++ wp-content/themes/cleanslate/header.php
@@ -40,6 +40,9 @@
         
         <!--[if lt IE 9]>
             <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+        <![endif]-->
+            
+        <!--[if lt IE 10]>
             <script src="<?php echo get_template_directory_uri(); ?>/css/ie.css" type="text/javascript"></script>
         <![endif]-->
         
