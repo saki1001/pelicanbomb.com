21356a2ff14fade69d587bda28735f4f8867d03d
Saki Sato <sakiisato@gmail.com>
2013-10-08 17:34:26 -0400

    added ie css

--- wp-content/themes/cleanslate/header.php
+++ wp-content/themes/cleanslate/header.php
@@ -40,6 +40,7 @@
         
         <!--[if lt IE 9]>
             <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+            <script src="<?php echo get_template_directory_uri(); ?>/css/ie.css" type="text/javascript"></script>
         <![endif]-->
         
         <?php wp_enqueue_scripts(); ?>
