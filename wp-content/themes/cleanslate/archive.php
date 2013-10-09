0a5972a408fec5a9385bb3d9010d0ee1e2a7e938
Saki Sato <sakiisato@gmail.com>
2013-10-09 15:06:46 -0400

    reformatted archive page

--- wp-content/themes/cleanslate/archive.php
+++ wp-content/themes/cleanslate/archive.php
@@ -10,48 +10,36 @@
  * @package CleanSlate
  * @since CleanSlate 0.1
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
     
     <?php if ( have_posts() ) : ?>
         
         <section id="content">
-            
-            <?php get_sidebar(); ?>
-            
-            <?php
-                
-                $count = 0;
-            ?>
             
             <div id="articles">
             
             <?php
                 while ( have_posts() ) : the_post();
                     
-                    if ( in_category('browse') ) :
-                        get_template_part( 'content', get_post_format() );
-                        
-                        $count++;
-                    else :
-                        // don't show
-                    endif;
+                    get_template_part( 'content-search', get_post_format() );
                     
                 endwhile;
-                
-                if ($count === 0) :
-                    echo '<p class="not-found">Sorry, no results found.</p>';
-                endif;
             ?>
             
             </div>
             
+            <?php get_sidebar(); ?>
+            
         </section>
         
-    <?php else :
-        // Content Not Found Template
-        include('content-not-found.php');
-        
-    endif; ?>
+    <?php
+        else :
+            // Content Not Found Template
+            include('content-not-found.php');
+            
+        endif;
+    ?>
     
 <?php get_footer(); ?>
