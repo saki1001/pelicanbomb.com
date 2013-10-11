f577b38039ebc7cc6d532d5ddae4a8899b51305d
Saki Sato <sakiisato@gmail.com>
2013-10-11 11:31:54 -0400

    php file to route calendar ajax request

--- wp-content/themes/cleanslate/php/get-adjacent-calendar.php
+++ wp-content/themes/cleanslate/php/get-adjacent-calendar.php
@@ -10,23 +10,11 @@
 <?php
     require_once('../../../../wp-load.php');
     
-    if( isset($_GET['cal_date']['post_date'])) {
-        
-        $cal_date = $_GET['cal_date'];
-        $post_date = $_GET['post_date'];
-        
-        // $current_year = mysql2date('Y', $cal_date);
-        // $current_month = mysql2date('m', $cal_date);
-        
+    if( isset($_GET['request_date']) ) {
+        $requestDate = $_GET['request_date'];
     } else {
-        $current_year = date('Y', current_time('timestamp'));
-        $current_month = date('m', current_time('timestamp'));
-        $current_day = date('d', current_time('timestamp'));
-        
-        $cal_date = $current_month.'-'.$current_day.'-'.$current_year;
-        $post_date = $cal_date;
+        $requestDate = date("Y-m-d");
     }
     
-    $category = '26';
-    get_calendar_custom($category, $cal_date, $post_date);
+    get_calendar_custom($requestDate);
 ?>
